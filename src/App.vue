b9af1e25c47d920043988fbfd8d6e22bb91812e6
BENCHAABANE Fahim <amoniack7@gmail.com>
2021-04-20 22:42:48 +0200

    Initialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Game/>
+  </div>
+</template>
+
+<script>
+  import Game from './components/Game.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Game
+    }
+  }
+</script>
+
+<style>
+  body {
+    margin:0;
+    padding: 0;
+    overflow: hidden;
+  }
+  #app {
+    font-family: Alice, 'Crush Shine', Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
